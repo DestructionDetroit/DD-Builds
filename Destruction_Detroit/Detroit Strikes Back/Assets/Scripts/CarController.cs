80b7ec29a34f4f1af4eeb4ea095c7ba1692ba593
16Mega <46603190+16Mega@users.noreply.github.com>
2019-01-11 09:51:05 -0600

    Added Assets

---
+++ Destruction_Detroit/Detroit Strikes Back/Assets/Scripts/CarController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public float speed;
+    public float curroty;
+    public float curposz;
+    public float curposx;
+    public float curposy;
+    public GameObject car;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        float curroty = car.transform.rotation.eulerAngles.y;
+        float curposz = car.transform.position.z;
+        float curposx = car.transform.position.x;
+        float curposy = car.transform.position.y;
+        if ((Input.GetKey(KeyCode.W)&&(!Input.GetKey(KeyCode.A)))||(Input.GetKey(KeyCode.W)&&(!Input.GetKey(KeyCode.D))))
+        {
+            transform.position += transform.forward * Time.deltaTime * speed;
+        }
+        if ((Input.GetKey(KeyCode.S) && (!Input.GetKey(KeyCode.A))) || (Input.GetKey(KeyCode.S) && (!Input.GetKey(KeyCode.D))))
+        {
+            transform.position -= transform.forward * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
+        {
+            rb.rotation = Quaternion.Euler(0, curroty - 2.5f, 0);
+            transform.position += transform.forward * Time.deltaTime * (speed-45.0f);
+        }
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W))
+        {
+            rb.rotation = Quaternion.Euler(0, curroty + 2.5f, 0);
+            transform.position += transform.forward * Time.deltaTime * (speed-45.0f);
+        }
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S))
+        {
+            rb.rotation = Quaternion.Euler(0, curroty + 2.5f, 0);
+            transform.position += transform.forward * Time.deltaTime * (speed - 45.0f);
+        }
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S))
+        {
+            rb.rotation = Quaternion.Euler(0, curroty - 2.5f, 0);
+            transform.position += transform.forward * Time.deltaTime * (speed - 45.0f);
+        }
+
+    }
+}
---
+++ Destruction_Detroit/Detroit Strikes Back/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public float speed;
+    public float curroty;
+    public float curposz;
+    public float curposx;
+    public float curposy;
+    public GameObject player;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        float curroty = player.transform.rotation.eulerAngles.y;
+        float curposz = player.transform.position.z;
+        float curposx = player.transform.position.x;
+        float curposy = player.transform.position.y;
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * speed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * Time.deltaTime * speed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * Time.deltaTime * speed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * Time.deltaTime * speed;
+        }
+        if (Input.GetKeyDown("e"))
+        {
+            rb.rotation = Quaternion.Euler(0, curroty + 90.0f, 0);
+        }
+        if (Input.GetKeyDown("q"))
+        {
+            rb.rotation = Quaternion.Euler(0, curroty - 90.0f, 0);
+        }
+
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            rb.velocity = new Vector3(0, 7, 0);
+        }
+    }
+}
